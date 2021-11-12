417e618df73a5695e6234659996ba34f29a6b528
Ayush <ayushsh2601@gmail.com>
2021-11-12 16:38:55 +0530

    Updates

---
+++ precache-manifest.eb8fe080362f16a401a7136063b9aed5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7170bf067df14cc67c064d4e963b2f4e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a6e83e4fa26d8162ab4d",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "258e27aa51454bcaae03",
+    "url": "/home/static/js/2.b89df85d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.b89df85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e83e4fa26d8162ab4d",
+    "url": "/home/static/js/main.5afac918.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "b383327a7d4b1e5e12bdc97b0b223445",
+    "url": "/home/static/media/android.b383327a.png"
+  },
+  {
+    "revision": "98ce2d8c8e5a0ab0a951137850f390bb",
+    "url": "/home/static/media/ayushsharma.98ce2d8c.png"
+  },
+  {
+    "revision": "8bc4ecd75c91c0ae82dce808263a8271",
+    "url": "/home/static/media/resume.8bc4ecd7.pdf"
+  },
+  {
+    "revision": "1e6047887cedc2e5bb7dde099cdc43d0",
+    "url": "/home/static/media/wheel.1e604788.png"
+  }
+]);
